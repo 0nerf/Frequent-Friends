2986bfe53f88d0efb9406a642ea18ce130dae825
0nerf <onerefe3556@gmail.com>
2026-07-19 03:26:02 +0300

    Update state.ts

--- state.ts
+++ state.ts
@@ -1,11 +1,3 @@
-/*
- * Vencord, a Discord client mod
- * Copyright (c) 2024 Vendicated and contributors
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-
-// Shared runtime state to avoid circular imports between index.tsx and ui.tsx
-
 let _isEnabled = false;
 export function isPluginEnabled() { return _isEnabled; }
 export function setIsEnabled(v: boolean) { _isEnabled = v; }
